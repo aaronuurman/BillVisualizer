3309c9e074283258a6b6892fc01f7ee0e9bb72e6
Aaron Uurman <aaronuurman@gmail.com>
2020-04-25 13:49:41 +0300

    Create shared resources for test projects

--- UnitTests/ResourcesFixture.cs
+++ UnitTests/ResourcesFixture.cs
@@ -12,7 +12,7 @@ namespace UnitTests
         public static string BillPdfPath => Path.Combine(ResourcesDir, "bill-33666222.pdf");
 
         /// <summary>Path to corrupted pdf bill.</summary>
-        public static string CorruptedPdfPath => Path.Combine(ResourcesDir, "corrupted_bill.pdf");
+        public static string CorruptedPdfPath => Path.Combine(ResourcesDir, "corrupted-bill.pdf");
 
         /// <summary>Path to image file.</summary>
         public static string ImagePath => Path.Combine(ResourcesDir, "notpdf.jpg");
---
+++ UnitTests/Services/ExcelToDataSetConverterTests.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace UnitTests.Services
+{
+    public class ExcelToDataSetConverterTests : IDisposable
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
