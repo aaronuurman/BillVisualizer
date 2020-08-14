85f0ce1c04ed261179bb8c0f147f0d2fe9e882bc
Aaron Uurman <aaronuurman@gmail.com>
2020-08-14 06:04:21 +0300

    Add ExcelToDataSetConverter tests

--- UnitTests/Services/ExcelToDataSetConverterTests.cs
+++ UnitTests/Services/ExcelToDataSetConverterTests.cs
@@ -1,10 +1,45 @@
 using System;
 using System.IO;
+using BillVisualizer.Services;
+using FluentAssertions;
+using Xunit;
 
 namespace UnitTests.Services
 {
     public class ExcelToDataSetConverterTests : ResourcesUnitTest
     {
+        private static string ExcelFilePath;
+        
+        public ExcelToDataSetConverterTests() {
+            new PdfToExcelConverter().Convert(Path.Combine(TempDataDir, ResourcesFixture.BillPdfPath));
+            ExcelFilePath = Path.Combine(TempDataDir, ResourcesFixture.ResourcesDir, "bill-33666222.xls");
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public async void Convert_ArgumentNullException_EmptyFilePath(string filePath)
+        {
+            // Arrange
+            var service = new ExcelToDataSetConverter();
+            
+            // Act && Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(() => service.Convert(filePath));
+        }
+
+        [Fact]
+        public async void Convert_Success_DataSetCreated()
+        {
+            // Arrange
+            var service = new ExcelToDataSetConverter();
+        
+            // Act
+            var result = await service.Convert(ExcelFilePath);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Tables[0].Rows.Count.Should().Be(73);
+        }
         
     }
 }
